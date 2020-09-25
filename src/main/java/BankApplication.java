1f964f3b03cd4034c17da82141197cae9b8908c0
oyushche <uhmateush@gmail.com>
2020-09-25 04:19:09 -0400

    task 1 finished

--- src/main/java/BankApplication.java
+++ src/main/java/BankApplication.java
@@ -10,6 +10,8 @@ import com.luxoft.bankapp.service.BankingImpl;
 import com.luxoft.bankapp.model.Client.Gender;
 import com.luxoft.bankapp.service.storage.ClientRepository;
 import com.luxoft.bankapp.service.storage.MapClientRepository;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class BankApplication {
 
@@ -18,8 +20,10 @@ public class BankApplication {
 
     public static void main(String[] args) {
 
-        ClientRepository repository = new MapClientRepository();
-        Banking banking = initialize(repository);
+        ApplicationContext context =
+                new ClassPathXmlApplicationContext("application-context.xml");
+
+        Banking banking = initialize(context);
 
         workWithExistingClients(banking);
 
@@ -100,10 +104,9 @@ public class BankApplication {
     /*
      * Method that creates a few clients and initializes them with sample values
      */
-    public static Banking initialize(ClientRepository repository) {
+    public static Banking initialize(ApplicationContext context) {
 
-        Banking banking = new BankingImpl();
-        banking.setRepository(repository);
+        Banking banking = (Banking) context.getBean("banking");
 
         Client client_1 = new Client(CLIENT_NAMES[0], Gender.MALE);
 
